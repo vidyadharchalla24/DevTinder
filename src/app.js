6389230ed0d29b25f7914c3abc8b2924ab13dadd
vidyadharchalla24 <vidyadharchalla333@gmail.com>
2025-05-23 17:03:32 +0530

    create mongoose model and signup api call

--- src/app.js
+++ src/app.js
@@ -1,18 +1,33 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./config/database");
 
 const app = express();
 
-app.use("/getAllData",(req,res)=>{
-    throw new Error("abc");
-    res.send("data sent");
+const User = require('./models/user');
+
+app.post('/signUp', async (req,res)=>{
+    //creating a new instance of the useer model
+    const user = new User({
+        firstName: "Vidya",
+        lastName: "Challa",
+        emailId: "vidya.challa3@gmail.com",
+        password: "Vidya#4234"
+    });
+
+    try {
+        await user.save();
+        res.send("User added successfully!!");
+    } catch (error) {
+        res.status(400).send("Error saving the user "+error.message);
+    }
+
 });
 
-app.use("/",(err,req,res,next)=>{
-    if(err){
-        res.status(500).send("Something went wrong!");
-    }
-})
-
-app.listen(4000,()=>{
+connectDB().then(() => {
+  console.log("Database connection established...");
+  app.listen(4000, () => {
     console.log("Server is successfully listening on port 4000...");
+  });
+}).catch(err=>{
+    console.error("Database connection failed to establish!!");
 });
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const connectDB = async ()=>{
+    await mongoose.connect(
+        "mongodb://localhost:27017/devTinder"
+    )
+};
+
+module.exports = connectDB;
