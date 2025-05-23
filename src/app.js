e5ac414402348e72cbca2eb452bbd20921c7821e
vidyadharchalla24 <vidyadharchalla333@gmail.com>
2025-05-23 13:02:39 +0530

    wildcard error-handling

--- src/app.js
+++ src/app.js
@@ -1,26 +1,17 @@
 const express = require('express');
 
 const app = express();
-const {authAdmin,authUser} = require('./middlewares/auth');
 
-app.use("/admin",authAdmin)
-
-app.get("/admin/getAllData",(req,res)=>{
-    
-    res.send("Gets all data!!");
+app.use("/getAllData",(req,res)=>{
+    throw new Error("abc");
+    res.send("data sent");
 });
 
-app.delete("/admin/deleteData",(req,res)=>{
-    res.send("data got deleted");
-});
-
-app.get("/user/data",authUser,(req,res)=>{
-    res.send("Data saved successfully!!");
-});
-
-app.get("/user/login",(req,res)=>{
-    res.send("Data saved successfully!!");
-});
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Something went wrong!");
+    }
+})
 
 app.listen(4000,()=>{
     console.log("Server is successfully listening on port 4000...");
